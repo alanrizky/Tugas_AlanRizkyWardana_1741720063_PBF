45a28b98ca092b86ae1553b8227a286d439d4caf
Alan Rizky Wardana <androidmaniac28@gmail.com>
2020-04-15 12:40:27 +0700

    Tugas 8 Selesai

---
+++ alan-minggu11/src/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8dvUCbKX51sPn1TRsD3ktDOKoguMAxHc",
+  authDomain: "fir-react-e22e6.firebaseapp.com",
+  databaseURL: "https://fir-react-e22e6.firebaseio.com",
+  projectId: "fir-react-e22e6",
+  storageBucket: "fir-react-e22e6.appspot.com",
+  messagingSenderId: "190245542083",
+  appId: "1:190245542083:web:bb2f6da562c0c073daaa76",
+  measurementId: "G-HX4ZW3V1TH",
+};
+
+export default firebaseConfig;
