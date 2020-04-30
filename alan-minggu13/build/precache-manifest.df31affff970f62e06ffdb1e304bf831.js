cfbfef744266d671aeddec5fd0a7ff2eb442f770
Alan Rizky Wardana <39402095+alanrizky@users.noreply.github.com>
2020-04-30 21:10:28 +0700

    Tugas 9 Selesai

---
+++ alan-minggu13/build/precache-manifest.df31affff970f62e06ffdb1e304bf831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e779031f53f89f883dfe626eb5bfa7cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bb4363c665c768527cc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7840aaffd013753050e",
+    "url": "/static/js/2.a2ba8385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2ba8385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb4363c665c768527cc",
+    "url": "/static/js/main.34ea9d6b.chunk.js"
+  },
+  {
+    "revision": "f493f7dfe92b6b05dbdb",
+    "url": "/static/js/runtime-main.aa928fa0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ alan-minggu13/src/firebase/config.js
@@ -0,0 +1,14 @@
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpd0HfsOcY3v0zOHdDgQGPVgCxmsKkX8M",
+  authDomain: "fir-login-6bae3.firebaseapp.com",
+  databaseURL: "https://fir-login-6bae3.firebaseio.com",
+  projectId: "fir-login-6bae3",
+  storageBucket: "fir-login-6bae3.appspot.com",
+  messagingSenderId: "844791486238",
+  appId: "1:844791486238:web:14a977e63d295b25e75b0f",
+};
+
+export default firebaseConfig;
